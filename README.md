35d4b2fa811f5b0465920f1146109bfe1c802da0
DeVaris Brown <devaris@devaris.com>
2012-12-17 16:47:03 -0800

    added readme and user operations

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 These are simple examples for using Python to consume the Zendesk API. Make sure your ZD_USER and ZD_PASS environment variables are set before using this.
 
+I've used the Requests library to do all my HTTP communication so if you need to do more complex operations check it out: <http://docs.python-requests.org/en/latest/>.
+
+In the code, I've done basic CRUD operations for Tickets and Users. If you'd like to see more, email me at <api@zendesk.com>. These should be pretty self explanatory.
+
+Make sure you change up all the variable names to your instance inside the code before running 
+
 Usage:
 
-Get Ticket
-
+```
+python zdTicketOperations.py
 ```
 
-```
-Create Ticket
+Sample Output:
 
 ```
-
+{"url":"https://thegizzle.zendesk.com/api/v2/users/311469541.json","id":311469541,"external_id":null,"name":"Pablo","alias":null,"created_at":"2012-12-17T23:42:48Z","updated_at":"2012-12-17T23:42:48Z","active":true,"verified":false,"shared":false,"locale_id":null,"locale":"en-US","time_zone":"Pacific Time (US & Canada)","last_login_at":null,"email":"pablito@example.org","phone":null,"signature":null,"details":null,"notes":null,"organization_id":null,"role":"end-user","custom_role_id":null,"moderator":false,"ticket_restriction":"requested","only_private_comments":false,"tags":[],"suspended":false,"photo":null}],"next_page":null,"previous_page":null,"count":13}
+{'status': '201 Created', 'x-zendesk-api-version': 'v2', 'content-length': '622', 'server': 'nginx/1.0.15', 'x-zendesk-user-id': '244792127', 'connection': 'keep-alive', 'location': 'https://thegizzle.zendesk.com/api/v2/users/309162917.json', 'cache-control': 'no-cache', 'date': 'Tue, 18 Dec 2012 00:37:11 GMT', 'x-zendesk-origin-server': 'app14.sys.zendesk.com', 'content-type': 'application/json; charset=utf-8', 'x-runtime': '489'}
+{"user":{"url":"https://thegizzle.zendesk.com/api/v2/users/309162917.json","id":309162917,"external_id":null,"name":"Zendesk Dev Team","alias":null,"created_at":"2012-12-18T00:37:10Z","updated_at":"2012-12-18T00:37:10Z","active":true,"verified":false,"shared":false,"locale_id":null,"locale":"en-US","time_zone":"Pacific Time (US & Canada)","last_login_at":null,"email":"api@zendesk.com","phone":null,"signature":null,"details":null,"notes":null,"organization_id":null,"role":"agent","custom_role_id":null,"moderator":false,"ticket_restriction":null,"only_private_comments":false,"tags":[],"suspended":false,"photo":null}}
+{'status': '200 OK', 'x-zendesk-api-version': 'v2', 'x-content-type-options': 'nosniff', 'content-encoding': 'gzip', 'transfer-encoding': 'chunked', 'x-runtime': '53', 'server': 'nginx/1.0.15', 'x-zendesk-user-id': '244792127', 'connection': 'keep-alive', 'etag': '"825947fe73243005f72e30ea47095d7d"', 'x-zendesk-origin-server': 'app34.sys.zendesk.com', 'cache-control': 'private, max-age=0, must-revalidate', 'date': 'Tue, 18 Dec 2012 00:37:11 GMT', 'content-type': 'application/json; charset=utf-8'}
+{"user":{"url":"https://thegizzle.zendesk.com/api/v2/users/244792127.json","id":244792127,"external_id":null,"name":"DeVaris Brown","alias":"Master of the Universe","created_at":"2012-08-06T19:56:20Z","updated_at":"2012-11-01T03:43:06Z","active":true,"verified":true,"shared":false,"locale_id":null,"locale":"en-US","time_zone":"America/Denver","last_login_at":"2012-12-18T00:36:06Z","email":"dbrown@zendesk.com","phone":"","signature":"You've just been helped by the Master...Your Master... Master doesn't have a phone number so don't even ask chump","details":"","notes":"","organization_id":22140487,"role":"admin","custom_role_id":null,"moderator":true,"ticket_restriction":null,"only_private_comments":false,"tags":["redirect_to_lotus"],"suspended":false,"photo":{"id":47898237,"file_name":"5542293400_5415a5334f_b.jpg","content_url":"https://thegizzle.zendesk.com/system/photos/4789/8237/5542293400_5415a5334f_b.jpg","content_type":"image/jpeg","size":1854,"thumbnails":[{"id":47898247,"file_name":"5542293400_5415a5334f_b_thumb.jpg","content_url":"https://thegizzle.zendesk.com/system/photos/4789/8237/5542293400_5415a5334f_b_thumb.jpg","content_type":"image/jpeg","size":1857}]}}}
 ```
--- zdTicketOperations.py
+++ zdTicketOperations.py
@@ -7,36 +7,65 @@ import requests, json, os
 ################################################################
 
 # Get Tickets
-get_url = 'https://thegizzle.zendesk.com/api/v2/tickets.json'
+get_tickets_url = 'https://thegizzle.zendesk.com/api/v2/tickets.json'
 headers = {'Accept':'application/json'}
-get = requests.get(get_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), headers=headers)
-print get.headers
-print get.text
+get_tickets = requests.get(get_tickets_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), headers=headers)
+print get_tickets.headers
+print get_tickets.text
 
 # Create Ticket
-create_url = 'https://thegizzle.zendesk.com/api/v2/tickets.json'
+create_ticket_url = 'https://thegizzle.zendesk.com/api/v2/tickets.json'
 headers = {'Accept':'application/json', 'Content-Type':'application/json'}
-create_data = {"ticket":{"subject":"My printer is on fire!", "comment": { "body": "The smoke is very colorful." }}}
-create = requests.post(create_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), data=json.dumps(create_data), headers=headers)
-print create.headers
-print create.text
+create_ticket_data = {"ticket":{"subject":"My printer is on fire!", "comment": { "body": "The smoke is very colorful." }}}
+create_ticket = requests.post(create_ticket_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), data=json.dumps(create_ticket_data), headers=headers)
+print create_ticket.headers
+print create_ticket.text
+
+# Show Ticket
+show_ticket_url = 'https://thegizzle.zendesk.com/api/v2/tickets/123.json'
+headers = {'Accept':'application/json'}
+show_ticket = requests.get(show_ticket_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), headers=headers)
+print show_ticket.headers
+print show_ticket.text
 
 # Update Ticket
-update_url = 'https://thegizzle.zendesk.com/api/v2/tickets/123.json'
+update_ticket_url = 'https://thegizzle.zendesk.com/api/v2/tickets/123.json'
 headers = {'Accept':'application/json', 'Content-Type':'application/json'}
-update_data = {"ticket":{"status":"pending","comment":{"public":"true", "body": "Thanks, this is now updated!"}}}
-update = requests.put(update_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), data=json.dumps(update_data), headers=headers)
-print update.headers
-print update.text
+update_ticket_data = {"ticket":{"status":"pending","comment":{"public":"true", "body": "Thanks, this is now updated!"}}}
+update_ticket = requests.put(update_ticket_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), data=json.dumps(update_ticket_data), headers=headers)
+print update_ticket.headers
+print update_ticket.text
 
 # Create Ticket w/ New Requester
-request_url = 'https://thegizzle.zendesk.com/api/v2/tickets.json'
+ticket_request_url = 'https://thegizzle.zendesk.com/api/v2/tickets.json'
 headers = {'Accept':'application/json', 'Content-Type':'application/json'}
-request_data = {"ticket": {"subject": "Hello","comment": { "body": "Some question" },"requester": { "locale_id": 8, "name": "Pablo", "email": "pablito@example.org" }}}
-request = requests.post(request_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), data=json.dumps(request_data), headers=headers)
-print request.headers
-print request.text
+ticket_request_data = {"ticket": {"subject": "Hello","comment": { "body": "Some question" },"requester": { "locale_id": 8, "name": "Pablo", "email": "pablito@example.org" }}}
+ticket_request = requests.post(ticket_request_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), data=json.dumps(ticket_request_data), headers=headers)
+print ticket_request.headers
+print ticket_request.text
 
 ################################################################
 ## USERS
 ################################################################
+
+# Get All Users
+get_users_url = 'https://thegizzle.zendesk.com/api/v2/users.json'
+headers = {'Accept':'application/json'}
+get_users = requests.get(get_users_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), headers=headers)
+print get_users.headers
+print get_users.text
+
+# Create User w/ Multiple Identities
+create_user_url = 'https://thegizzle.zendesk.com/api/v2/users.json'
+headers = {'Accept':'application/json', 'Content-Type':'application/json'}
+create_user_data = {"user": {"name": "Zendesk Dev Team", "role":"agent", "identities": [{ "type": "email", "value": "api@zendesk.com"}, {"type": "twitter", "value": "zendeskdevteam" }]}}
+create_user = requests.post(create_user_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), data=json.dumps(create_user_data), headers=headers)
+print create_user.headers
+print create_user.text
+
+# Show User
+show_user_url = 'https://thegizzle.zendesk.com/api/v2/users/244792127.json'
+headers = {'Accept':'application/json'}
+show_user = requests.get(show_user_url, auth=(os.environ['ZD_USER'], os.environ['ZD_PASS']), headers=headers)
+print show_user.headers
+print show_user.text
